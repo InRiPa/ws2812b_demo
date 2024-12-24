b1c524be2b3a85c093cac85de82d15f4ffb055f3
Ingolf Gehrhardt <gehingo@freenet.de>
2024-12-24 23:33:32 +0100

    Added some white led rings example stuff

--- hutscape.c
+++ hutscape.c
@@ -93,6 +93,32 @@ void run_oneLed()
     }
 }
 
+int rings[5][2] = {{0,23},{24,39},{40,51},{52,59},{60}};
+
+void run_crazy(uint8_t ring)
+{
+  const int num_leds = 64;
+  
+    const uint32_t color_white = urgb_u32(0xff, 0xff, 0xff); // BRIGHT WHITE
+    int led_count = rings[ring][1] - rings[ring][0];
+    for (int j = 0; j < num_leds; j++)
+    {
+      if (ring == 4){
+        put_pixel(color_white); // Turn on LED at position i
+      }
+      else{
+        if (rings[ring][1] >= j && rings[ring][0] <= j){
+          put_pixel(color_white); // Turn on LED at position i
+        }
+        else
+        {
+          put_pixel(urgb_u32(0, 0, 0)); // Turn off other LEDs
+        }
+      }
+    }
+    
+}
+
 int main()
 {
     stdio_init_all();
@@ -103,9 +129,16 @@ int main()
     char str[12];
 
     ws2812_program_init(pio, sm, offset, PIN_TX, 800000, false);
-
+    int ring = 0;
     while (true)
     {
-        run_oneLed();
+        //run_oneLed();
+        run_crazy(ring);
+        if (ring < 5) {
+          ring++;
+        }else {
+          ring=0;
+        }
+        sleep_ms(200); // Delay to create the running effect
     }
 }
